783360108d5b9156ffad9caf20427ba9c4c2559c
Jigar Patel <jigarxp@gmail.com>
2022-09-20 22:57:47 -0400

    added login and register with brcrypt and jwt cookies

---
+++ server/controllers/userController.js
@@ -0,0 +1,82 @@
+const db = require('../models/model');
+const userController = {};
+const bcrypt = require('bcryptjs');
+
+const SALT_WORK_FACTOR = 10;
+
+userController.createUser = async (req, res, next) => {
+  if (!req.body.username)
+    return next({
+      log: null,
+      status: 400,
+      message: 'Username required',
+    });
+  if (!req.body.password)
+    return next({
+      log: null,
+      status: 400,
+      message: 'Password required',
+    });
+
+  try {
+    const hash = await bcrypt.hash(req.body.password, SALT_WORK_FACTOR);
+    const values = [req.body.username, hash];
+    const userQuery = `
+      INSERT INTO users(username, password)
+      VALUES($1, $2)
+      RETURNING _id, username;
+    `;
+    const user = await db.query(userQuery, values);
+    res.locals.user = {
+      _id: user.rows[0]._id,
+      username: user.rows[0].username,
+    };
+    return next();
+  } catch (e) {
+    return next({
+      log: null,
+      status: 500,
+      message:
+        'An error occured creating the user. This username may already exist!',
+    });
+  }
+};
+
+userController.verifyUser = async (req, res, next) => {
+  try {
+    const values = [req.body.username];
+    const userQuery = `
+      SELECT * 
+      FROM users
+      WHERE username=$1;
+    `;
+    const user = await db.query(userQuery, values);
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.rows[0].password
+    );
+
+    if (validPassword) {
+      res.locals.user = {
+        _id: user.rows[0]._id,
+        username: user.rows[0].username,
+      };
+      return next();
+    }
+    return next({
+      log: null,
+      status: 500,
+      message: {
+        err: 'ERROR: Invalid Username or password',
+      },
+    });
+  } catch (err) {
+    return next({
+      log: 'userController.verifyUser ERROR: ' + err,
+      status: 500,
+      message: { err: 'ERROR: Error verifying user' },
+    });
+  }
+};
+
+module.exports = userController;
--- server/models/model.js
+++ server/models/model.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
-
-const PG_URI = 'postgres://drivmmdq:dOrUoYjYc18tWPvker0q2wcNbB6BRuGm@jelani.db.elephantsql.com/drivmmdq';
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
-    connectionString: PG_URI
-})
+  connectionString: process.env.PG_URI,
+});
 
 module.exports = {
-    query: (text, params, callback) => {
-        console.log('query executed', text);
-        return pool.query(text, params, callback);
-    }
-}
+  query: (text, params, callback) => {
+    console.log('query executed', text);
+    return pool.query(text, params, callback);
+  },
+};
